36a79d305aae3d0197f67bdc7f07794d4b04d4dc
MyatKaungKyaw <myatkaungg31@gmail.com>
2023-10-12 23:14:10 +0630

    create theme and text

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Main from './src/components/main';
+import Main from './src/components/Main';
 
 export default function App() {
   return <Main />
---
+++ src/components/Text.tsx
@@ -0,0 +1,48 @@
+import { Text as NativeText, StyleSheet, TextStyle } from 'react-native';
+
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+  text: {
+    color: theme.colors.textPrimary,
+    fontSize: theme.fontSizes.body,
+    fontFamily: theme.fonts.main,
+    fontWeight: theme.fontWeights.normal,
+  },
+  colorTextSecondary: {
+    color: theme.colors.textSecondary,
+  },
+  colorPrimary: {
+    color: theme.colors.primary,
+  },
+  fontSizeSubheading: {
+    fontSize: theme.fontSizes.subheading,
+  },
+  fontWeightBold: {
+    fontWeight: theme.fontWeights.bold,
+  },
+});
+
+interface TextProps {
+  color: string | undefined;
+  fontSize: string | undefined;
+  fontWeight: string | undefined;
+  style: Object;
+  [index: string]: any | undefined;
+}
+
+
+const Text = ({ color, fontSize, fontWeight, style, ...props }: TextProps) => {
+  const textStyle = [
+    styles.text,
+    color === 'textSecondary' && styles.colorTextSecondary,
+    color === 'primary' && styles.colorPrimary,
+    fontSize === 'subheading' && styles.fontSizeSubheading,
+    fontWeight === 'bold' && styles.fontWeightBold,
+    style,
+  ];
+
+  return <NativeText style={textStyle} {...props} />;
+};
+
+export default Text;
