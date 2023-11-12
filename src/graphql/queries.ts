29be1501ebb0d35c8bc622b82e62a58ee68a4e3a
Myat Kaung Kyaw <myatkaungg31@gmail.com>
2023-11-12 21:11:18 +0630

    use graphql in useRepositories hook

---
+++ src/graphql/queries.ts
@@ -0,0 +1,32 @@
+import { gql } from "@apollo/client";
+
+export const repositories = gql`
+  query {
+    repositories {
+      totalCount
+      pageInfo {
+        hasPreviousPage
+        hasNextPage
+        startCursor
+        endCursor
+      }
+      edges {
+        cursor
+        node {
+          id
+          name
+          ownerName
+          createdAt
+          fullName
+          reviewCount
+          ratingAverage
+          forksCount
+          stargazersCount
+          description
+          language
+          ownerAvatarUrl
+        }
+      }
+    }
+  }
+`;
--- src/hooks/useRepositories.ts
+++ src/hooks/useRepositories.ts
@@ -1,25 +1,16 @@
-import { useState, useEffect } from "react";
 import { RepositoryListType } from "../components/Repository/types";
+import { useQuery } from "@apollo/client";
+import { repositories as getRepos } from "../graphql/queries";
 
 const useRepositories = () => {
-  const [repositories, setRepositories] = useState<RepositoryListType>();
-  const [loading, setLoading] = useState(false);
+  const { loading, data, refetch } = useQuery(getRepos);
+  let repositories: RepositoryListType | undefined;
 
-  const fetchRepositories = async () => {
-    setLoading(true);
+  if (!loading && Object.hasOwn(data, "repositories")) {
+    repositories = data.repositories as RepositoryListType;
+  }
 
-    const response = await fetch("http://10.1.9.144:5000/api/repositories");
-    const json = (await response.json()) as RepositoryListType;
-
-    setLoading(false);
-    setRepositories(json);
-  };
-
-  useEffect(() => {
-    fetchRepositories();
-  }, []);
-
-  return { repositories, loading, refetch: fetchRepositories };
+  return { repositories, loading, refetch };
 };
 
 export default useRepositories;
