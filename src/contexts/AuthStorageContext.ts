edfe9703fddf5657034e11cce0041aa81d96c013
Myat Kaung Kyaw <myatkaungg31@gmail.com>
2023-11-14 15:42:00 +0630

    implement AuthStorageContext

---
+++ src/contexts/AuthStorageContext.ts
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+import AuthStorage from "../utils/authStorage";
+
+const AuthStorageContext = createContext<AuthStorage | null>(null);
+
+export default AuthStorageContext;
