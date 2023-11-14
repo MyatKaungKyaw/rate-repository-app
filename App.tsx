1f5741a4bca10dc8248cd967079b9a7d3b607dbb
Myat Kaung Kyaw <myatkaungg31@gmail.com>
2023-11-14 15:43:56 +0630

    add token in header ApolloClient from storage

--- App.tsx
+++ App.tsx
@@ -1,27 +1,22 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import Main from "./src/components/Main";
-import { NativeRouter } from "react-router-native";
 import { ApolloProvider } from "@apollo/client";
-import createApolloClient from "./src/utils/apolloClient";
+import { NativeRouter } from "react-router-native";
 
-const client = createApolloClient();
+import Main from "./src/components/Main";
+import AuthStorageContext from "./src/contexts/AuthStorageContext";
+import createApolloClient from "./src/utils/apolloClient";
+import AuthStorage from "./src/utils/authStorage";
+
+const authStorage = new AuthStorage();
+const client = createApolloClient(authStorage);
 
 export default function App() {
   return (
     <NativeRouter>
       <ApolloProvider client={client}>
-        <Main />
+        <AuthStorageContext.Provider value={authStorage}>
+          <Main />
+        </AuthStorageContext.Provider>
       </ApolloProvider>
     </NativeRouter>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- src/utils/apolloClient.ts
+++ src/utils/apolloClient.ts
@@ -1,5 +1,8 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import Constants from "expo-constants";
+
+import AuthStorage from "./authStorage";
 
 const uri = Constants.expoConfig?.extra && Constants.expoConfig.extra.apolloUri;
 
@@ -7,9 +10,23 @@ const httpLink = createHttpLink({
   uri,
 });
 
-const createApolloClient = () => {
+const createApolloClient = (authStorage: AuthStorage) => {
+  const authLink = setContext(async (_, { headers }) => {
+    try {
+      const accessToken = await authStorage.getAccessToken();
+      return {
+        headers: {
+          ...headers,
+          authorization: accessToken ? `Bearer ${accessToken}` : "",
+        },
+      };
+    } catch (e) {
+      console.log(e);
+      return { headers };
+    }
+  });
   return new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
   });
 };
